d248e81627c5888b4a264f0d03dfcb50512977e2
Nausicaah <nausicraaft@gmail.com>
2019-01-26 18:16:56 +0100

    ajout des input hors forfait

--- controleurs/c_validerFrais.php
+++ controleurs/c_validerFrais.php
@@ -90,41 +90,6 @@ switch ($action) {
         $lesFraisForfait = $pdo->getLesFraisForfait($idVisiteur, $idMois);
         include 'vues/v_listeFraisForfaitC.php';
         include 'vues/v_listeFraisHorsForfaitC.php';
-
         break;
 
-    case 'supprimerFrais':
-    $idFrais = filter_input(INPUT_GET, 'idFrais', FILTER_SANITIZE_STRING);
-    $pdo->supprimerFraisHorsForfait($idFrais);
-    
-     //Récupère infos visiteurs sélectionné
-        $lesVisiteurs = $pdo->getLesVisiteurs();
-        $visiteurASelectionner = filter_input(INPUT_POST, 'lstVisiteurs', FILTER_SANITIZE_STRING);
-
-        //Récupère infos mois sélectionné
-        $lesMois = $pdo->getLesMoisDisponibles($visiteurASelectionner);
-        $moisASelectionner = filter_input(INPUT_POST, 'lstMoisC', FILTER_SANITIZE_STRING);
-        include 'vues/v_listeMoisC.php';
-
-        //Récupération d'informations à afficher
-        $idVisiteur = $visiteurASelectionner;
-        $idMois = $moisASelectionner;
-        $numAnnee = substr($idMois, 0, 4);
-        $numMois = substr($idMois, 4, 2);
-        $nom = $pdo->getNom($idVisiteur);
-        $prenom = $pdo->getPrenom($idVisiteur);
-
-        //Utilisation des fonctions afin de pouvoir afficher les données sélectionnées
-        $lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idVisiteur, $idMois);
-        $lesInfosFicheFrais = $pdo->getLesInfosFicheFrais($idVisiteur, $idMois);
-        $nbJustificatifs = $pdo->getNbJustificatifs($idVisiteur, $idMois);
-        $lesFraisForfait = $pdo->getLesFraisForfait($idVisiteur, $idMois);
-
-        //Récupération des nouveaux frais
-        $lesFrais = filter_input(INPUT_POST, 'lesFrais', FILTER_DEFAULT, FILTER_FORCE_ARRAY);
-        $pdo->majFraisForfait($idVisiteur, $idMois, $lesFrais);
-        $lesFraisForfait = $pdo->getLesFraisForfait($idVisiteur, $idMois);
-        include 'vues/v_listeFraisForfaitC.php';
-        include 'vues/v_listeFraisHorsForfaitC.php';
-    break;
 }
