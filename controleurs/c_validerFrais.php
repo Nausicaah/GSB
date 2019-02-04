91245c46e9569c7702de4851a7ccf22c8e1f3590
Nausicaah <nausicraaft@gmail.com>
2019-02-04 17:37:06 +0100

    validation de fiche (fonctions à faire)

--- controleurs/c_validerFrais.php
+++ controleurs/c_validerFrais.php
@@ -260,7 +260,42 @@ switch ($action) {
     /**
      * Permet de valider la fiche de frais
      */
-    case 'validerfiche':
+    case 'validerFiche':
+        
+        $lesVisiteurs = $pdo->getLesVisiteurs();
+        $visiteurASelectionner = filter_input(INPUT_POST, 'lstVisiteurs', FILTER_SANITIZE_STRING);
+        //Récupère infos mois sélectionné
+        $lesMois = $pdo->getLesMoisDisponibles($visiteurASelectionner);
+        $moisASelectionner = filter_input(INPUT_POST, 'lstMoisC', FILTER_SANITIZE_STRING);
+        include 'vues/v_listeMoisC.php';
+
+        //Récupération d'informations à afficher
+        $idVisiteur = $visiteurASelectionner;
+        $idMois = $moisASelectionner;
+        $numAnnee = substr($idMois, 0, 4);
+        $numMois = substr($idMois, 4, 2);
+        $nom = $pdo->getNom($idVisiteur);
+        $prenom = $pdo->getPrenom($idVisiteur);
+
+        //Utilisation des fonctions afin de pouvoir afficher les données sélectionnées
+        $lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idVisiteur, $idMois);
+        $lesFraisForfait = $pdo->getLesFraisForfait($idVisiteur, $idMois);
+        $lesInfosFicheFrais = $pdo->getLesInfosFicheFrais($idVisiteur, $idMois);
+        $nbJustificatifs = $pdo->getNbJustificatifs($idVisiteur, $idMois);
+        include 'vues/v_listeFraisForfaitC.php';
+        include 'vues/v_listeFraisHorsForfaitC.php';
+        include 'vues/v_listeNbJustificatifsC.php';
+        
+        $totalFF = $pdo->getTotalFraisForfait($idVisiteur, $idMois);
+        $totalFHF = $pdo->getTotalFraisHorsForfait($idVisiteur, $idMois);
+        
+        $totalFiche = $totalFF + $totalFHF;
+
+        $pdo->majMontantFicheValide($idVisiteur, $leMois, $sommeValidee);
+
+        
+        $etat = 'VA';
+        $pdo->majEtatFicheFrais($idVisiteur, $leMois, $etat);
 
         break;
 }
--- vues/v_listeNbJustificatifsC.php
+++ vues/v_listeNbJustificatifsC.php
@@ -22,8 +22,22 @@
               role="form">
             <fieldset>
              <input type="text" id="nbJustificatifs" value="<?php echo $nbJustificatifs ?>" name="nbJustificatifs" class="form-control">  
-             <button class="btn btn-success" type="submit">Ajouter</button>
-             <button class="btn btn-danger" type="submit">Réinitialiser</button>
+             <div class="col-md-4"><button class="btn btn-success" type="submit">Ajouter</button>
+            <input name="lstVisiteurs" value="<?php echo $visiteurASelectionner; ?>" type="hidden">
+            <input name="lstMoisC" value="<?php echo $moisASelectionner; ?>" type="hidden">
+            </fieldset>
+        </form>
+    </div>
+</div>
+<div class="row">    
+    <h3>Valider fiche</h3>
+    <div class="col-md-4">
+        <form method="post" 
+              action="index.php?uc=validerFrais&action=validerFiche" 
+              role="form">
+            <fieldset>
+                <a class="btn btn-success" type="submit" href="index.php?uc=validerFrais&action=validerFiche" 
+                onclick="return confirm('Voulez-vous vraiment valider la fiche de frais ?');">Valider fiche de frais</a>
             <input name="lstVisiteurs" value="<?php echo $visiteurASelectionner; ?>" type="hidden">
             <input name="lstMoisC" value="<?php echo $moisASelectionner; ?>" type="hidden">
             </fieldset>
